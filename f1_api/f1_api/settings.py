791db7344c108f1694dc697c93008fdde15ffed0
p2vvel <p2vvel@gmail.com>
2022-04-18 22:41:57 +0200

    Create 'api' app, create requirements.txt

--- f1_api/f1_api/settings.py
+++ f1_api/f1_api/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'f1_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'f1db',
+        'USER': 'root',
+        'PASSWORD': 'djangodjango##',
+        'HOST': '127.0.0.1',
+        'PORT': '8080',
     }
 }
 
