bf004e2ea59bc42145101b17ffde3c96e5953c1d
p2vvel <p2vvel@gmail.com>
2022-05-15 02:02:41 +0200

    Caching and throttling working

---
+++ f1_api/api/mixins.py
@@ -0,0 +1,36 @@
+from django.core.cache import cache
+from rest_framework.response import Response
+from typing import Callable
+
+def process_request_or_cache(throttled_function: Callable[..., Response], request, *args, **kwargs) -> Response:
+    """
+    Check if result of function given as parameter has already been cached,
+    if yes, return cache reponse with cache content, else - save function 
+    result to cache and return response with this data
+
+    Args:
+        throttled_function (Callable[..., Response]): function to process or get result from cache
+        request: request
+
+    Returns:
+        Response: result of given function - processed or retrieved from cache
+    """
+    url_path = request.get_full_path()      # get request url (used as cache key)
+    cache_data = cache.get(url_path)        # try fetching data from cache
+    if cache_data:
+        # return Response with data from cache
+        return Response(cache_data)
+    else:
+        # data not found in cache - save it
+        response = throttled_function(request, *args, **kwargs)
+        cache.set(url_path, response.data)                  # cache result
+        return response         # return requested data
+
+
+class ReadOnlyThrottledMixin:
+    def list(self, request, *args, **kwargs):
+        return process_request_or_cache(super().list, request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        return process_request_or_cache(super().retrieve, request, *args, **kwargs)
+
--- f1_api/api/views.py
+++ f1_api/api/views.py
@@ -4,25 +4,28 @@ from rest_framework import viewsets
 from api.models import Circuits, Drivers, Constructors, Races, Seasons
 from api.serializers import DriverSerializer, ConstructorSerializer, CircuitSerializer, RaceSerializer, SeasonSerializer
 # from api.throttling import ViewThrottle
+from rest_framework.response import Response
+
+from django.core.cache import cache
+from api.mixins import ReadOnlyThrottledMixin
 
 
-class DriversViewset(viewsets.ReadOnlyModelViewSet):
+class DriversViewset(ReadOnlyThrottledMixin, viewsets.ReadOnlyModelViewSet):
     queryset = Drivers.objects.all()
     serializer_class = DriverSerializer
 
-
-class ConstructorsViewset(viewsets.ReadOnlyModelViewSet):
+class ConstructorsViewset(ReadOnlyThrottledMixin, viewsets.ReadOnlyModelViewSet):
     queryset = Constructors.objects.all()
     serializer_class = ConstructorSerializer
 
-class CircuitsViewset(viewsets.ReadOnlyModelViewSet):
+class CircuitsViewset(ReadOnlyThrottledMixin, viewsets.ReadOnlyModelViewSet):
     queryset = Circuits.objects.all()
     serializer_class = CircuitSerializer
 
-class RacesViewset(viewsets.ReadOnlyModelViewSet):
+class RacesViewset(ReadOnlyThrottledMixin, viewsets.ReadOnlyModelViewSet):
     queryset = Races.objects.all()
     serializer_class = RaceSerializer
 
-class SeasonsViewset(viewsets.ReadOnlyModelViewSet):
+class SeasonsViewset(ReadOnlyThrottledMixin, viewsets.ReadOnlyModelViewSet):
     queryset = Seasons.objects.all()
     serializer_class = SeasonSerializer
